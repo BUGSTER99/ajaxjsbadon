0f1a0a9a550f80767bede82d1503f75faf3eca70
Muhammad Arlan <arlan.diawan21@gmail.com>
2019-08-01 17:57:20 +0700

    badon

--- kjikbgi2019.sql
+++ kjikbgi2019.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2019 at 11:58 AM
+-- Generation Time: Aug 01, 2019 at 12:56 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -25,6 +25,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `email`
+--
+
+CREATE TABLE `email` (
+  `id_email` int(11) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `to_users` varchar(200) NOT NULL,
+  `email_content` text NOT NULL,
+  `email_sender` varchar(200) NOT NULL,
+  `status_` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `email`
+--
+
+INSERT INTO `email` (`id_email`, `subject`, `to_users`, `email_content`, `email_sender`, `status_`) VALUES
+(1, 'test', 'admin@mail.com', 'test', 'andi@mail.com', 0),
+(2, 'test', 'andi@mail.com', 'test', 'admin@mail.com', 0),
+(3, 'test2', 'admin@mail.com', 'test2', 'andi@mail.com', 0),
+(4, 'test2', 'andi@mail.com', 'test2', 'admin@mail.com', 0),
+(5, 'test2', 'admin@mail.com', 'test2', 'andi@mail.com', 0),
+(6, 'admin gans', 'admin@mail.com', 'hai', 'andi@mail.com', 0),
+(7, 'testt', 'andi@mail.com', 'testt', 'andi@mail.com', 0),
+(8, '123', 'admin@mail.com', '123', 'andi@mail.com', 0),
+(9, '123', 'admin@mail.com', '123', 'andi@mail.com', 0),
+(10, 'andi', 'andi@mail.com', 'andi', 'admin@mail.com', 0),
+(11, 'admin', 'admin@mail.com', '1234', 'andi@mail.com', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -41,24 +73,17 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_users`, `name`, `email`, `password`) VALUES
 (1, 'Admin', 'admin@mail.com', '21232f297a57a5a743894a0e4a801fc3'),
-(2, '', '[\'admin@mail.com\',\'admin\']', ''),
-(3, '', '\"[\'admin@mail.com\',\'admin\']\"', ''),
-(4, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(5, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(6, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(7, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(8, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(9, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(10, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(11, '', '{\"email\":\"A\",\"pass\":\"B\"}', ''),
-(12, 'andi', 'andi@mail.com', '123\r\n'),
-(13, 'kona', '123', '123\r\n'),
-(14, 'ibnu', '12', '12'),
-(15, 'kol', 'kol', '123');
+(2, 'Andi', 'andi@mail.com', 'ce0e5bf55e4f71749eade7a8b95c4e46');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `email`
+--
+ALTER TABLE `email`
+  ADD PRIMARY KEY (`id_email`);
 
 --
 -- Indexes for table `users`
@@ -71,10 +96,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `email`
+--
+ALTER TABLE `email`
+  MODIFY `id_email` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
